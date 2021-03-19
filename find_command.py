fcff48597a280b6589109ae2190d7c33fc9319a8
Bdubs <brent.wipf@gmail.com>
2021-03-19 15:58:23 -0500

    Moved find_command.py into find_keywords.py.
    Fixed find_keywords.py to call both functions at the end.
    
    Fixed paranthetical error in line 40 of find_keywords.py.

--- find_command.py
+++ find_command.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 
-
 def find_commands(keywords, phrase):
   if 'weather' in keywords:
     if 'week' in keywords:
--- find_keywords.py
+++ find_keywords.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python3
 
-"""Test Variables"""
-string = 'hey Russell, where does your name come from?'
+string = 'Hey Russell, where does your name come from?'
 
 keywords = []
+phrase = ''
 dull_words = ['hey', 'Russell', 'does', 'come']
+new_questions = ./new_questions.txt
+
 def find_keywords(string):
-    for word in string:
-        if word in dull_words:
-            continue
-        keywords.append(word)
-    print(keywords)
+  phrase = string.replace(',', '')
+  words = phrase.split()
+  for word in string:
+    if word not in dull_words:
+      keywords.append(word)
+  return phrase
+  return keywords
+
+
+def find_commands(keywords, phrase):
+  if 'weather' in keywords:
+    if 'week' in keywords:
+      print('This week looks beautiful!')
+    elif 'tomorrow' in keywords:
+      print('Tomorrow will be similar to today.')
+    else:
+      print('Currently it is Sunny with a high of 75 degrees.')
+  elif 'time' in keywords:
+    if 'meeting' or 'appointment' in keywords:
+      print('Your next event is at 6pm')
+    else:
+      print('The current time is {}'.format(ctime(time())))
+  elif 'your name' in phrase:
+    print('I am named after my maker\'s grandfather')
+  else:
+    print('I am unsure of the answer to your inquiry.')
+    print('I will make note of it so that I can answer it next time.')
+    with open(new_questions, 'a') as f:
+      f.write(phrase)
+
+
+find_keywords(string)
+find_commands(keywords, phrase)
