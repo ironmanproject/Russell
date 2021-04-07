e3e38e338a341b79031fedfce9082ef4b0affe92
Bdubs <brent.wipf@gmail.com>
2021-04-07 14:50:54 -0500

    Removed size restaints. Restored back to full-screen.

--- ims_interface.py
+++ ims_interface.py
@@ -16,8 +16,8 @@ resolution_string, junk = p2.communicate()
 resolution = resolution_string.split()[0]
 resolution = resolution.decode('utf-8')
 width, height = resolution.split('x')
-width = int(width)-75
-depth = int(height)-70
+width = int(width)
+depth = int(height)
 
 # Initialize pygame
 pygame.init()
